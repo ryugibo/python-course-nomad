644d5ab7946c042f25a824b75b8bfb69dbaaccb9
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-15 00:39:29 +0000

    :memo: #1.8 | Code Challenge!

--- main.py
+++ main.py
@@ -1,5 +1,78 @@
-def say_hello(name, age, are_from, fav_food):
-  return f"Hello {name} you are {age} years old you are from {are_from} you like {fav_food}"
+def is_number(a):
+  if type(a) is int:
+    return True
 
-hello = say_hello(name = "gibo", age = "18", fav_food = "kimchi", are_from = "korea")
-print(hello)
+  if type(a) is str and a.isdigit():
+    return True
+
+  return False
+
+def plus(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) + int(b)
+  else:
+    return None
+  
+def minus(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) - int(b)
+  else:
+    return None
+  
+def times(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) * int(b)
+  else:
+    return None
+  
+def division(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) / int(b)
+  else:
+    return None
+  
+def negation(a):
+  if is_number(a):
+    return -int(a)
+  else:
+    return None
+  
+def power(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) ** int(b)
+  else:
+    return None
+  
+def remainder(a, b):
+  if is_number(a) and is_number(b):
+    return int(a) % int(b)
+  else:
+    return None
+
+print(f" 3  +  7 : {plus(3, 7)}")
+print(f"\"3\" +  7 : {plus('3', 7)}")
+print(f" 3  + \"7\": {plus(3, '7')}")
+
+print(f" 3  -  7 : {minus(3, 7)}")
+print(f"\"3\" -  7 : {minus('3', 7)}")
+print(f" 3  - \"7\": {minus(3, '7')}")
+
+print(f" 3  *  7 : {times(3, 7)}")
+print(f"\"3\" *  7 : {times('3', 7)}")
+print(f" 3  * \"7\": {times(3, '7')}")
+
+print(f" 3  /  7 : {division(3, 7)}")
+print(f"\"3\" /  7 : {division('3', 7)}")
+print(f" 3  / \"7\": {division(3, '7')}")
+
+
+print(f"  -3 : {negation(3)}")
+print(f" -\"3\": {negation('3')}")
+
+print(f" 3  **  7 : {power(3, 7)}")
+print(f"\"3\" **  7 : {power('3', 7)}")
+print(f" 3  ** \"7\": {power(3, '7')}")
+
+print(f" 3  %  7 : {remainder(3, 7)}")
+print(f"\"3\" %  7 : {remainder('3', 7)}")
+print(f" 3  % \"7\": {remainder(3, '7')}")
