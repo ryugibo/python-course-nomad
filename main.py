72c39345c669fde9a8a4a8618eae5ae5d7b53383
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-15 00:16:18 +0000

    :memo: #1.7 | Keyworded Arguments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def plus(a, b):
-  return a + b
+def say_hello(name, age, are_from, fav_food):
+  return f"Hello {name} you are {age} years old you are from {are_from} you like {fav_food}"
 
-result = plus(2, 4)
-
-print(result)
+hello = say_hello(name = "gibo", age = "18", fav_food = "kimchi", are_from = "korea")
+print(hello)
