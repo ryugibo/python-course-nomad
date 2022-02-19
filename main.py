b046f598a49a02c0b8feb690c794c8762eeea6fe
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-19 12:24:52 +0000

    :memo: #2.5 | Requesting Each Page

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+last_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-links = pagination.find_all("a")
-pages = []
-for link in links[:-1]:
-  pages.append(int(link.string))
-
-max_page = pages[-1]
+indeed_jobs = extract_indeed_jobs(last_page)
